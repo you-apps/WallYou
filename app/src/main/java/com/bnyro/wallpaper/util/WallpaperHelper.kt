a2926ffb78e468ead763164a4724a876055c58f9
Bnyro <bnyro@tutanota.com>
2022-10-21 15:20:18 +0200

    Center resize the bitmap

--- app/src/main/java/com/bnyro/wallpaper/util/WallpaperHelper.kt
+++ app/src/main/java/com/bnyro/wallpaper/util/WallpaperHelper.kt
@@ -4,6 +4,7 @@ import android.app.WallpaperManager
 import android.content.Context
 import android.graphics.Bitmap
 import android.os.Build
+import android.util.DisplayMetrics
 import androidx.annotation.RequiresApi
 import com.bnyro.wallpaper.constants.WallpaperMode
 
@@ -29,20 +30,56 @@ object WallpaperHelper {
 
     fun setWallpaper(context: Context, bitmap: Bitmap, mode: Int) {
         Thread {
+            val resizedBitmap = getResizedBitmap(bitmap, context.resources.displayMetrics)
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
                 if (mode in listOf(WallpaperMode.BOTH, WallpaperMode.HOME)) {
-                    setWallpaperUp(context, bitmap, WallpaperManager.FLAG_SYSTEM)
+                    setWallpaperUp(context, resizedBitmap, WallpaperManager.FLAG_SYSTEM)
                 }
                 if (mode in listOf(WallpaperMode.BOTH, WallpaperMode.LOCK)) {
-                    setWallpaperUp(context, bitmap, WallpaperManager.FLAG_LOCK)
+                    setWallpaperUp(context, resizedBitmap, WallpaperManager.FLAG_LOCK)
                 }
             } else {
-                setWallpaperLegacy(context, bitmap)
+                setWallpaperLegacy(context, resizedBitmap)
             }
         }.start()
     }
 
-    fun getResizedBitmap(bitmap: Bitmap): Bitmap {
-        return Bitmap.createBitmap(bitmap)
+    fun getResizedBitmap(bitmap: Bitmap, displayMetrics: DisplayMetrics): Bitmap {
+        var resizedBitmap = bitmap
+        val screenWidth = displayMetrics.widthPixels
+        val screenHeight = displayMetrics.heightPixels
+        val bitmapWidth = resizedBitmap.width.toFloat()
+        val bitmapHeight = resizedBitmap.height.toFloat()
+
+        val bitmapRatio = (bitmapWidth / bitmapHeight)
+        val screenRatio = (screenWidth / screenHeight)
+        val bitmapNewWidth: Int
+        val bitmapNewHeight: Int
+
+        if (screenRatio > bitmapRatio) {
+            bitmapNewWidth = screenWidth
+            bitmapNewHeight = (bitmapNewWidth / bitmapRatio).toInt()
+        } else {
+            bitmapNewHeight = screenHeight
+            bitmapNewWidth = (bitmapNewHeight * bitmapRatio).toInt()
+        }
+
+        resizedBitmap = Bitmap.createScaledBitmap(
+            resizedBitmap,
+            bitmapNewWidth,
+            bitmapNewHeight,
+            true
+        )
+
+        val bitmapGapX: Int = ((bitmapNewWidth - screenWidth) / 2.0f).toInt()
+        val bitmapGapY: Int = ((bitmapNewHeight - screenHeight) / 2.0f).toInt()
+
+        return Bitmap.createBitmap(
+            resizedBitmap,
+            bitmapGapX,
+            bitmapGapY,
+            screenWidth,
+            screenHeight
+        )
     }
 }
