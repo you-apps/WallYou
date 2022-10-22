70fe643a95d8de3a46393cf6c736456dddb431ce
Bnyro <bnyro@tutanota.com>
2022-10-22 09:39:24 +0200

    Add Preferences backend

---
+++ app/src/main/java/com/bnyro/wallpaper/util/PrefHolder.kt
@@ -0,0 +1,15 @@
+package com.bnyro.wallpaper.util
+
+import android.content.Context
+import android.content.SharedPreferences
+
+object PrefHolder {
+    private const val prefFile = "preferences"
+    lateinit var Preferences: SharedPreferences
+    val PrefEditor: SharedPreferences.Editor
+        get() = Preferences.edit()
+
+    fun init(context: Context) {
+        Preferences = context.getSharedPreferences(prefFile, Context.MODE_PRIVATE)
+    }
+}
