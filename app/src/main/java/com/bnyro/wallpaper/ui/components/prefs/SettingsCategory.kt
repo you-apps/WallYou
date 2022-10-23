c3a030f23e197f4f25b6f7972a73c8d0a63ea0b9
Bnyro <bnyro@tutanota.com>
2022-10-23 08:55:00 +0200

    improved settings categories

--- app/src/main/java/com/bnyro/wallpaper/ui/components/prefs/SettingsCategory.kt
+++ app/src/main/java/com/bnyro/wallpaper/ui/components/prefs/SettingsCategory.kt
@@ -3,6 +3,7 @@ package com.bnyro.wallpaper.ui.components.prefs
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -21,8 +22,9 @@ fun SettingsCategory(
             )
     ) {
         Text(
-            text = title,
-            fontSize = 12.sp
+            text = title.uppercase(),
+            fontSize = 10.sp,
+            color = MaterialTheme.colorScheme.secondary
         )
     }
 }
