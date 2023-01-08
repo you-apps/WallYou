c8699acff19960369f72496108f9096baecd4b93
Bnyro <bnyro@tutanota.com>
2023-01-08 18:52:57 +0100

    Update navigation drawer icons

--- app/src/main/java/com/bnyro/wallpaper/ui/nav/DrawerScreens.kt
+++ app/src/main/java/com/bnyro/wallpaper/ui/nav/DrawerScreens.kt
@@ -1,12 +1,12 @@
 package com.bnyro.wallpaper.ui.nav
 
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Air
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Info
-import androidx.compose.material.icons.filled.Launch
+import androidx.compose.material.icons.filled.Landscape
 import androidx.compose.material.icons.filled.Pix
 import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material.icons.filled.Wallpaper
 import androidx.compose.ui.graphics.vector.ImageVector
 import com.bnyro.wallpaper.R
 
@@ -16,10 +16,10 @@ sealed class DrawerScreens(
     val icon: ImageVector,
     val divideBefore: Boolean = false
 ) {
-    object Wallhaven : DrawerScreens(R.string.wallhaven, "wh", Icons.Default.Wallpaper)
+    object Wallhaven : DrawerScreens(R.string.wallhaven, "wh", Icons.Default.Landscape)
     object Picsum : DrawerScreens(R.string.picsum, "ps", Icons.Default.Pix)
 
-    object OWalls : DrawerScreens(R.string.owalls, "ow", Icons.Default.Launch)
+    object OWalls : DrawerScreens(R.string.owalls, "ow", Icons.Default.Air)
     object Favorites : DrawerScreens(R.string.favorites, "favorites", Icons.Default.Favorite, true)
     object Settings : DrawerScreens(R.string.settings, "settings", Icons.Default.Settings, true)
     object About : DrawerScreens(R.string.about, "about", Icons.Default.Info)
