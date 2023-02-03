7254b78dfefb8789e5dad3ae6285d024c4a555a8
Bnyro <bnyro@tutanota.com>
2023-02-03 15:17:52 +0100

    Fix that settings are not scrollable (closes #47)

--- app/src/main/java/com/bnyro/wallpaper/ui/pages/SettingsPage.kt
+++ app/src/main/java/com/bnyro/wallpaper/ui/pages/SettingsPage.kt
@@ -7,6 +7,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -61,9 +63,11 @@ fun SettingsPage(
         }
     }
 
+    val scrollState = rememberScrollState()
     Column(
         modifier = Modifier
             .fillMaxSize()
+            .verticalScroll(scrollState)
     ) {
         AboutContainer {
             Column {
