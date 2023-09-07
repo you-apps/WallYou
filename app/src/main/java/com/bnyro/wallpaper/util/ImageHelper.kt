61c14a36e8557babc78158aab10f93cdfcc3166d
Bnyro <bnyro@tutanota.com>
2023-09-07 22:05:07 +0200

    fix: rotate local images by exif data if provided (closes #123)

--- app/src/main/java/com/bnyro/wallpaper/util/ImageHelper.kt
+++ app/src/main/java/com/bnyro/wallpaper/util/ImageHelper.kt
@@ -3,8 +3,10 @@ package com.bnyro.wallpaper.util
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.Matrix
 import android.net.Uri
 import androidx.core.graphics.drawable.toBitmap
+import androidx.exifinterface.media.ExifInterface
 import coil.ImageLoader
 import coil.annotation.ExperimentalCoilApi
 import coil.request.ImageRequest
@@ -50,11 +52,46 @@ object ImageHelper {
             .data(url)
             .allowHardware(false)
     }
+    private fun rotateBitmap(bitmap: Bitmap, exifInterface: ExifInterface): Bitmap {
+        val orientation = runCatching {
+            exifInterface.getAttributeInt(
+                ExifInterface.TAG_ORIENTATION,
+                ExifInterface.ORIENTATION_NORMAL
+            )
+        }.getOrElse { return bitmap }
+
+        return when (orientation) {
+            ExifInterface.ORIENTATION_ROTATE_90, ExifInterface.ORIENTATION_TRANSPOSE -> rotateImage(
+                bitmap,
+                90f
+            )
+            ExifInterface.ORIENTATION_ROTATE_180, ExifInterface.ORIENTATION_FLIP_VERTICAL -> rotateImage(
+                bitmap,
+                180f
+            )
+            ExifInterface.ORIENTATION_ROTATE_270, ExifInterface.ORIENTATION_TRANSVERSE -> rotateImage(
+                bitmap,
+                -90f
+            )
+            else -> bitmap
+        }
+    }
+
+    private fun rotateImage(bitmap: Bitmap, rotation: Float): Bitmap {
+        val matrix = Matrix().apply {
+            setScale(-1f, 1f)
+            setRotate(rotation)
+        }
+        val oriented = Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, matrix, true)
+        bitmap.recycle()
+        return oriented
+    }
 
     fun getLocalImage(context: Context, imagePath: Uri): Bitmap? {
-        context.contentResolver.openInputStream(imagePath)?.use {
-            return BitmapFactory.decodeStream(it)
+        return context.contentResolver.openInputStream(imagePath)?.use { stream ->
+            val exifInterface = ExifInterface(stream)
+            val bitmap = exifInterface.thumbnailBitmap ?: return null
+            rotateBitmap(bitmap, exifInterface)
         }
-        return null
     }
 }
