b810288ddccb01af3c7ad7c600574f31f61c31ca
Bnyro <bnyro@tutanota.com>
2023-09-18 09:02:52 +0200

    feat(reddit): support for time filters and sort order

--- app/src/main/java/com/bnyro/wallpaper/api/re/ReApi.kt
+++ app/src/main/java/com/bnyro/wallpaper/api/re/ReApi.kt
@@ -7,13 +7,15 @@ import com.bnyro.wallpaper.util.RetrofitBuilder
 class ReApi : CommunityApi() {
     override val name = "Reddit"
     override val baseUrl = "https://www.reddit.com/"
+    override val filters: Map<String, List<String>> = mapOf(
+        "sort" to listOf("top", "new", "hot", "rising"),
+        "time" to listOf("month", "year", "hour", "day", "week")
+    )
 
     val api = RetrofitBuilder.create(baseUrl, Reddit::class.java)
 
     override val defaultCommunityName = "r/wallpaper"
 
-    private val sort = "top" // TODO: move to filter dialog by overriding [tags]
-    private val time = "week" // TODO: move to filter dialog by overriding [tags]
     private val imageRegex = Regex("^.+\\.(jpg|jpeg|png|webp)\$")
 
     private var nextPageAfter: String? = null
@@ -26,7 +28,7 @@ class ReApi : CommunityApi() {
         if (page == 1) nextPageAfter = null
         val communityName = getCommunityName().replaceFirst("r/", "")
 
-        val response = api.getRedditData(communityName, sort, time, nextPageAfter)
+        val response = api.getRedditData(communityName, getQuery("sort"), getQuery("time"), nextPageAfter)
         // needed in order to load the next page
         nextPageAfter = response.data?.after
 
