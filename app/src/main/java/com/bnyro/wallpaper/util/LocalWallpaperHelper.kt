a7fae1f101e8499b7d016c9298f283fdbc84e443
Bnyro <bnyro@tutanota.com>
2024-01-30 14:20:36 +0100

    feat: recursive local wallpapers and filter for images only

--- app/src/main/java/com/bnyro/wallpaper/util/LocalWallpaperHelper.kt
+++ app/src/main/java/com/bnyro/wallpaper/util/LocalWallpaperHelper.kt
@@ -6,9 +6,27 @@ import androidx.documentfile.provider.DocumentFile
 import com.bnyro.wallpaper.enums.WallpaperConfig
 
 object LocalWallpaperHelper {
+    private fun getAllFilesInCurrentDir(directory: DocumentFile?): List<DocumentFile> {
+        val files = mutableListOf<DocumentFile>()
+
+        val contents = directory?.listFiles().orEmpty()
+        files.addAll(contents.filter { it.isFile })
+
+        contents.filter { it.isDirectory }.forEach {
+            files.addAll(getAllFilesInCurrentDir(it))
+        }
+
+        return files
+    }
+
+    private val DocumentFile.isImage get() = type?.startsWith("image/") ?: false
+
     fun getLocalWalls(context: Context, config: WallpaperConfig): List<DocumentFile> {
         return config.localFolderUris.map {
-            DocumentFile.fromTreeUri(context, it.toUri())?.listFiles().orEmpty().toList()
-        }.flatten()
+            val dir = DocumentFile.fromTreeUri(context, it.toUri())
+            getAllFilesInCurrentDir(dir)
+        }
+            .flatten()
+            .filter { it.isImage }
     }
 }
