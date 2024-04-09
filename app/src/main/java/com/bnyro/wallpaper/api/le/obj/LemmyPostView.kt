1a081dd54eabf4fc9eefbccca1d87d6d539c9c93
Bnyro <bnyro@tutanota.com>
2024-04-09 21:38:58 +0200

    fix: remove unused lemmy parameters (closes #185)

--- app/src/main/java/com/bnyro/wallpaper/api/le/obj/LemmyPostView.kt
+++ app/src/main/java/com/bnyro/wallpaper/api/le/obj/LemmyPostView.kt
@@ -5,13 +5,6 @@ import com.fasterxml.jackson.annotation.JsonProperty
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class LemmyPostView(
-    @JsonProperty("community") val community: LemmyCommunity = LemmyCommunity(),
     @JsonProperty("creator") val creator: LemmyCreator = LemmyCreator(),
-    @JsonProperty("creator_banned_from_community") val creatorBannedFromCommunity: Boolean = false,
-    @JsonProperty("creator_blocked") val creatorBlocked: Boolean = false,
     @JsonProperty("post") val post: LemmyPost = LemmyPost(),
-    @JsonProperty("read") val read: Boolean = false,
-    @JsonProperty("saved") val saved: Boolean = false,
-    @JsonProperty("subscribed") val subscribed: String = "",
-    @JsonProperty("unread_comments") val unreadComments: Int = 0
 )
