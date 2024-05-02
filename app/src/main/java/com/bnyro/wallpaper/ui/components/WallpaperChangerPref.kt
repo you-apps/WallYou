c974bbd6a3dfd74e5e69233113b6a660cc12fe05
Bnyro <bnyro@tutanota.com>
2024-05-02 09:34:55 +0200

    chore: remove unused imports

--- app/src/main/java/com/bnyro/wallpaper/ui/components/WallpaperChangerPref.kt
+++ app/src/main/java/com/bnyro/wallpaper/ui/components/WallpaperChangerPref.kt
@@ -2,16 +2,12 @@ package com.bnyro.wallpaper.ui.components
 
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.compose.animation.Crossfade
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.Button
@@ -19,7 +15,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
