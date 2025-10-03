690ee1dc7fcd749f0fca5e35fdcd7183688c3e29
Bnyro <bnyro@tutanota.com>
2025-10-03 12:52:48 +0200

    feat: support for predictive back gestures

--- app/src/main/java/com/bnyro/wallpaper/ui/components/FullscreenDialog.kt
+++ app/src/main/java/com/bnyro/wallpaper/ui/components/FullscreenDialog.kt
@@ -1,20 +1,57 @@
 package com.bnyro.wallpaper.ui.components
 
+import androidx.activity.compose.PredictiveBackHandler
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
+import androidx.compose.ui.window.DialogWindowProvider
+import kotlinx.coroutines.CancellationException
 
 @Composable
 fun FullscreenDialog(onDismissRequest: () -> Unit, content: @Composable () -> Unit) {
+    var alpha by remember {
+        mutableFloatStateOf(1f)
+    }
+
     Dialog(
-        onDismissRequest = onDismissRequest,
-        properties = remember {
-            DialogProperties(
-                usePlatformDefaultWidth = false,
-                decorFitsSystemWindows = false
-            )
-        },
-        content = content
-    )
+        properties = DialogProperties(
+            usePlatformDefaultWidth = false,
+            decorFitsSystemWindows = false
+        ),
+        onDismissRequest = onDismissRequest
+    ) {
+        PredictiveBackHandler { progress ->
+            try {
+                progress.collect { p ->
+                    alpha = 1 - 1.3f * p.progress
+                }
+                onDismissRequest.invoke()
+            } catch (_: CancellationException) {
+                alpha = 1f
+            }
+        }
+
+        val dialogWindowProvider = LocalView.current.parent as DialogWindowProvider
+        LaunchedEffect(dialogWindowProvider) {
+            dialogWindowProvider.window.setDimAmount(0f)
+        }
+
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+                .alpha(alpha)
+        ) {
+            content.invoke()
+        }
+    }
 }
