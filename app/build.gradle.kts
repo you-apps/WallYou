b54071676fc9dcecb9a15cfa211d5ce18d992469
Bnyro <bnyro@tutanota.com>
2026-06-19 10:39:18 +0200

    chore: bump version to v15.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.bnyro.wallpaper"
         minSdk = 23
         targetSdk = 35
-        versionCode = 39
-        versionName = "15.1"
+        versionCode = 40
+        versionName = "15.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
