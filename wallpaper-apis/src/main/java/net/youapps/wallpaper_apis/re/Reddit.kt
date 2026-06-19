a9d47087cde678a9ec9db03da22d66b09f107791
Suhas Dissanayake <suhasdissa@gmail.com>
2026-06-19 16:12:05 +0530

    fix: reddit unauthorized api issue

--- wallpaper-apis/src/main/java/net/youapps/wallpaper_apis/re/Reddit.kt
+++ wallpaper-apis/src/main/java/net/youapps/wallpaper_apis/re/Reddit.kt
@@ -1,17 +1,16 @@
 package net.youapps.wallpaper_apis.re
 
-import net.youapps.wallpaper_apis.re.obj.RedditResp
+import okhttp3.ResponseBody
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
 
-
 interface Reddit {
-    @GET("r/{subreddit}/{sort}.json")
+    @GET("r/{subreddit}/{sort}.rss")
     suspend fun getRedditData(
         @Path("subreddit") subreddit: String,
         @Path("sort") sort: String,
         @Query("t") time: String? = null,
         @Query("after") after: String? = null
-    ): RedditResp
+    ): ResponseBody
 }
--- wallpaper-apis/src/main/java/net/youapps/wallpaper_apis/re/obj/Children.kt
+++
@@ -1,9 +0,0 @@
-package net.youapps.wallpaper_apis.re.obj
-
-import kotlinx.serialization.SerialName
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class Children(
-    @SerialName("data") val childData: ChildData
-)
